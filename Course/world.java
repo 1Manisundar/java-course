d34731dd441255ad86812057bb5db3728f24e8f4
1Manisundar <chsundar143@gmail.com>
2026-08-17 07:43:25 +0530

    Day-1: Introduction to Java basics
    - Explained JVM, JRE, and JDK architecture
    - Covered class structure and public static void main (p.s.v.m)
    - Covered System.out.println (s.o.pln) and primitive types

---
+++ Course/world.java
@@ -0,0 +1,16 @@
+class World
+{
+    public static void main(String a[]) {
+        System.out.println("Hehe just printing bro");  
+
+        int num1 = 10;
+        int num2 = 5;
+
+        int result = num1+num2;
+
+        System.out.println(result);
+
+        //literals done
+
+    }
+}
